3f31b295d58b3809dc63176cf8d27d85ab724993
James Alan Bush <jamesalanbush1972@gmail.com>
2020-03-19 12:42:10 -0400

    Adds playback for tone barrier scores on the Apple Watch

---
+++ ToneBarrier3D WatchKit Extension/ToneGenerator.h
@@ -0,0 +1,34 @@
+//
+//  ToneGenerator.h
+//  ToneBarrier3D
+//
+//  Created by Xcode Developer on 2/1/20.
+//  Copyright © 2020 James Bush. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <AVFoundation/AVFoundation.h>
+#import <UIKit/UIKit.h>
+
+
+NS_ASSUME_NONNULL_BEGIN
+
+@protocol ToneGeneratorDelegate <NSObject>
+
+@required
+- (IBAction)play;
+
+@end
+
+@interface ToneGenerator : NSObject
+
++ (nonnull ToneGenerator *)sharedGenerator;
+
+@property (nonatomic, readonly) AVAudioEngine * _Nonnull audioEngine;
+@property (weak) id<ToneGeneratorDelegate> delegate;
+
+- (void)play;
+
+@end
+
+NS_ASSUME_NONNULL_END
